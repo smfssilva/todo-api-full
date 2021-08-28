46f9293e97ea1eb3b8d7ee634a93242442caa128
Silva <smfssilva@yahoo.com>
2021-08-28 01:08:31 -0300

    6

--- src/main/java/io/github/smfssilva/todo/TodoApplication.java
+++ src/main/java/io/github/smfssilva/todo/TodoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoApplication {
+//	<java.version>11</java.version>
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
